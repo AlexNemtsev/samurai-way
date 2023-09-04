0c7366fab4733c637543347a214e1dae47151011
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-04 07:44:57 +0300

    feat: add base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello, samurai! Let's go!</div>;
+  return (
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn"
+          alt=""
+        />
+      </header>
+      <nav className="nav">
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div className="content"></div>
+    </div>
+  );
 }
 
 export default App;
