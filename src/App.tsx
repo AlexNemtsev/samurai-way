dcdf87ecc8439276110be8ebee96bc7c02dc6612
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-07 17:51:27 +0300

    feat: lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,38 @@ function App() {
         />
       </header>
       <nav className="nav">
-        <div>Profile</div>
-        <div>Messages</div>
+        <ul>
+          <li>
+            <a href="">Profile</a>
+          </li>
+          <li>
+            <a href="">Messages</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">Music</a>
+          </li>
+          <li>
+            <a href="">Settings</a>
+          </li>
+        </ul>
       </nav>
-      <div className="content"></div>
+      <div className="content">
+        <img
+          src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg "
+          alt="background"
+        />
+        <div>ava + desc</div>
+        <div>
+          my posts <div>New post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
