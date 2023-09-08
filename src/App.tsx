798b9cd5389b4060a4fef17980d15ddca23bbc07
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-09-08 16:28:16 +0300

    feat: lesson 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-        <img
-          src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn"
-          alt=""
-        />
-      </header>
-      <nav className="nav">
-        <ul>
-          <li>
-            <a href="">Profile</a>
-          </li>
-          <li>
-            <a href="">Messages</a>
-          </li>
-          <li>
-            <a href="">News</a>
-          </li>
-          <li>
-            <a href="">Music</a>
-          </li>
-          <li>
-            <a href="">Settings</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="content">
-        <img
-          src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg "
-          alt="background"
-        />
-        <div>ava + desc</div>
-        <div>
-          my posts <div>New post</div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+function Profile() {
+  return (
+    <div className="content">
+      <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg " alt="background" />
+      <div>ava + desc</div>
+      <div>
+        my posts <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
